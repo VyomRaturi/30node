53169f171cf44f1fb177ffed167043110e66504a
Vyom Raturi <vyomraturi2004@gmail.com>
2024-02-15 10:10:29 +0530

    day15 done

---
+++ day15/ans.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+function loggingMiddleware(req, res, next) {
+    console.log("Timestamp:", new Date().toISOString()),
+        console.log("Method:", req.method),
+        console.log("URL:", req.url),
+        console.log("Headers:", req.headers),
+        console.log("Body:", req.body);
+    next();
+}
+
+app.use(loggingMiddleware);
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+});
