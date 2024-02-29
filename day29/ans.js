d2de5fe56aca481985d796897a88b2fc4505fc9c
Vyom Raturi <vyomraturi2004@gmail.com>
2024-02-29 18:56:26 +0530

    day29 done

---
+++ day29/ans.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  throw new Error("Something went wrong!");
+});
+
+function errorHandler(err, req, res, next) {
+  console.error(err.stack); // Log error stack trace to the console
+
+  // Send error details to the client
+  res.status(500).json({
+    status: "error",
+    message: err.message,
+  });
+}
+
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
